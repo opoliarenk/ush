c6ffe124b426707a3e5c5d5f7e8f64065a52faa5
Nazar <53997458+NazarWild@users.noreply.github.com>
2020-03-11 20:36:16 +0200

    add echo

--- src/mx_builtin_echo.c
+++ src/mx_builtin_echo.c
@@ -1,49 +1,49 @@
 #include "../inc/ush.h"
 
-// static char *change_str(char *str) {
-// 	char *dup = mx_strnew(strlen(str));
-// 	int i = 0;
-
-// 	while (str[i] != '\0') {
-// 		dup[i] = str[i] == '\n' ? 'n' : str[i];
-// 		i++;
-// 	}
-// 	dup[i] = '\0';
-// 	return dup;
-// }
+typedef struct s_echo{
+    bool stop;
+    bool n;
+    bool e;
+    bool E;
+}              t_echo;
 
 void mx_builtin_echo(char **arr) {
-	bool fl_n = false;
-	//bool fl_E = false;
-	int i = 1;
-	char *buf;
+    int i = 1;
+    int j = 0;
+    t_echo *echo = (t_echo *)malloc(sizeof(t_echo));
 
-	while (arr[i]) {
-		if (strcmp(arr[i], "-n") == 0)
-			fl_n = true;
-		// else if (strcmp(arr[i], "-e") == 0)
-		// 	fl_E = false;
-		// else if (strcmp(arr[i], "-E") == 0)
-		// 	fl_E = true;
-		else
-			break;
-		i++;		
-	}
-	while (arr[i + 1]) {
-		//if (fl_E)
-		//	buf = change_str(arr[i]);
-		//else
-			buf = arr[i];
-		mx_printstr(buf);
-		mx_printchar(32);
-		i++;
-		//if (fl_E)
-		//	mx_strdel(&buf);
-	}
-	mx_printstr(buf);
-	if (!fl_n)
-		mx_printchar(10);
-	else {
-		mx_printstr("%\n");
-	}
+    memset(echo, 0, sizeof(t_echo));
+    while (arr[i]) {
+        if (arr[i][j] == '-') {
+            while (arr[i][j]) {
+                if (arr[i][j] == 'n')
+                    echo->n = 1;
+                if (arr[i][j] == 'e') {
+                    echo->e = 1;
+                }
+                if (arr[i][j] == 'E') {
+                    echo->E = 1;
+                }
+                j++;
+            }
+        }
+        else
+            break;
+        i++;
+    }
+    if (mx_strlen_for_2star(arr) > 2) {
+        while (arr[i + 1]) {
+            mx_printstr(arr[i]);
+            mx_printchar(32);
+            i++;
+        }
+        mx_printstr(arr[i]);
+        if (echo->n)
+            mx_printstr("%\n");
+        else
+            mx_printchar(10);
+    }
+    else
+        if (!echo->n)
+            mx_printstr("\n");
 }

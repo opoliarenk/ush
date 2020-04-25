22d9962f42d6d616c2ad92e0988bc411ae83262c
Olena Poliarenko <opoliarenk@e2r8p7.unit.ua>
2020-04-26 00:57:35 +0300

    sleep

--- src/mx_unknown.c
+++ src/mx_unknown.c
@@ -2,7 +2,7 @@
 
 void mx_unknown(char **temp) {
     if (temp[0][5] == '=') {
-        dprintf(2, "ush: %s not found\n", &temp[0][6]);
+        dprintf(2, "ush: %s not found\n", temp[0]);
         exit(1);
     }
     else if (temp[0][5] == '$' && temp[0][6] != '$') {
@@ -12,6 +12,7 @@ void mx_unknown(char **temp) {
         if (temp[0][i] == '=' && temp[0][i + 1] != '=')
             exit(0);
     }
-    dprintf(2, "ush: command not found : %s\n", &temp[0][5]);
+    dprintf(2, "ush: command not found : %s\n", temp[0]);
     exit(127);
 }
+

8c17c0ebadcc3a5db5a30b4d7b229c1ec9893ebb
ndykyy <ndykyy@student.ucode.world>
2020-04-25 13:55:34 -0700

    echo changed

--- src/mx_builtin_echo.c
+++ src/mx_builtin_echo.c
@@ -24,6 +24,28 @@ static void func_for_slesh(char *str, int j) {
     mx_printchar(output);
 }
 
+static void print_1_type(char *arr, int j, int i) {
+    while (arr[j + i]) {
+        mx_printchar(arr[j]);
+        j++;
+    }
+}
+
+static void print_2_type(char *arr) {
+    int j = 1;
+
+    while (arr[j + 1]) {
+        if ((arr[j] == '\\' && arr[j + 1] != '\'')
+            || (arr[i][j] == '\\' && arr[i][j + 1] != '"')) {
+            func_for_slesh(arr, j + 1);
+            j++;
+        }
+        else
+            mx_printchar(arr[j]);
+        j++;
+    }
+}
+
 void mx_builtin_echo(char **arr, char *origin) {
     int i = 1;
     int j = 1;
@@ -35,60 +57,22 @@ void mx_builtin_echo(char **arr, char *origin) {
     i = mx_parser_4_echo(arr, echo);
     while (arr[i]) {
         if (echo->E) {
-            if (arr[i][0] == '\'') {
-                j = 1;
-                while (split[point][j] != '\'') {
-                    mx_printchar(split[point][j]);
-                    j++;
-                }
-            }
-            else if (arr[i][0] == '"') {
-                j = 1;
-                while (arr[i][j] != '"') {
-                    mx_printchar(arr[i][j]);
-                    j++;
-                }
-            }
-            else {
-                j = 0;
-                while (arr[i][j]) {
-                    mx_printchar(arr[i][j]);
-                    j++;
-                }
-            }
+            if (arr[i][0] == '\'')
+                print_1_type(split[point], 1, 1);
+            else if (arr[i][0] == '"')
+                print_1_type(arr[i], 1, 1);
+            else
+                print_1_type(arr[i], 0, 0);
         }
         else {
             if (arr[i][0] == '\'') {
-                j = 1;
-                while (arr[i][j] != '\'') {
-                    if (arr[i][j] == '\\'&& arr[i][j + 1] != '\'') {
-                        func_for_slesh(arr[i], j + 1);
-                        j++;
-                    }
-                    else
-                        mx_printchar(arr[i][j]);
-                    j++;
-                }
+                print_2_type(arr[i]);
             }
             else if (arr[i][0] == '"') {
-                j = 1;
-                while (arr[i][j] != '"') {
-                    if (arr[i][j] == '\\' && arr[i][j + 1] != '"') {
-                        func_for_slesh(arr[i], j + 1);
-                        j++;
-                    }
-                    else
-                        mx_printchar(arr[i][j]);
-                    j++;
-                }
+                print_2_type(arr[i]);
             }
-            else {
-                j = 0;
-                while (arr[i][j]) {
-                    mx_printchar(arr[i][j]);
-                    j++;
-                }
-            }
+            else
+                print_1_type(arr[i], 0, 0);
         }
         if (mx_strlen_for_2star(arr) - i != 1)
             mx_printchar(32);

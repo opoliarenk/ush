e7c7395100ec90aa1caa2b1f095ca766445dd8f5
Olena Poliarenko <opoliarenk@e2r12p1.unit.ua>
2020-05-05 22:34:10 +0300

    quotes_error

--- src/mx_check_echo.c
+++ src/mx_check_echo.c
@@ -1,43 +1,41 @@
 #include "../inc/ush.h"
 
-static int first(char *line, int i) {
-    i++;
-    for (; line[i] != '\x22'; i++) {
-        if (line[i] == '\0') {
+static int first(char *line, int *i) {
+    (*i)++;
+    for (; line[(*i)] != '\x22'; (*i)++) {
+        if (line[(*i)] == '\0') {
             dprintf(2, "Odd number of quotes.\n");
             return 0;
         }
-        if (line[i] == '\x5c')
-            i++;
+        if (line[(*i)] == '\x5c')
+            (*i)++;
     }
     return 1;
 }
 
-static int second(char *line, int i) {
-    i++;
-    for (; line[i] != '\x27'; i++) {
-        if (line[i] == '\0') {
+static int second(char *line, int *i) {
+    (*i)++;
+    for (; line[(*i)] != '\x27'; (*i)++) {
+        if (line[(*i)] == '\0') {
             dprintf(2, "Odd number of quotes.\n");
             return 0;
         }
-        if (line[i] == '\x5c')
-            i++;
+        if (line[(*i)] == '\x5c')
+            (*i)++;
     }
     return 1;
 }
 
 int mx_check_echo(char *line) {
-    int i = 0;
-
-    while (line[i] && line[i] != '\x22' && line[i] != '\x27')
-        i++;
-    if (line[i] == '\x22') {
-            if (!first(line, i))
+    for (int i = 0; line[i]; i++) {
+        if (line[i] == '\x22') {
+            if (!first(line, &i))
                 return 0;
         }
         else if (line[i] == '\x27') {
-            if (!second(line, i))
+            if (!second(line, &i))
                 return 0;
         }
+    }
     return 1;
 }
--- src/mx_unknown.c
+++ src/mx_unknown.c
@@ -1,5 +1,42 @@
 #include "../inc/ush.h"
 
+static void first(char *line, int *i) {
+    (*i)++;
+    for (; line[(*i)] != '\x22'; (*i)++) {
+        if (line[(*i)] == '\x5c') {}
+            // (*i)++;
+        else {
+            dprintf(2, "%c", line[(*i)]);
+        }
+    }
+}
+
+static void second(char *line, int *i) {
+    (*i)++;
+    for (; line[(*i)] != '\x27'; (*i)++) {
+        if (line[(*i)] == '\x5c') {}
+            // (*i)++;
+        else {
+            dprintf(2, "%c", line[(*i)]);
+        }
+    }
+}
+
+static void if_quote(char *line) {
+    for (int i = 0; line[i]; i++) {
+        if (line[i] == '\x22') {
+            first(line, &i);
+        }
+        else if (line[i] == '\x27') {
+            second(line, &i);
+        }
+        else {
+            dprintf(2, "%c", line[i]);
+        }
+    }
+    dprintf(2, "\n");
+}
+
 void mx_unknown(char **temp) {
     if (temp[0][0] == '$' && temp[0][1] != '$') {
         exit(0);
@@ -8,7 +45,8 @@ void mx_unknown(char **temp) {
         if (temp[0][i] == '=')
             return;
     }
-    dprintf(2, "ush: command not found: %s\n", temp[0]);
+    dprintf(2, "ush: command not found: ");
+    if_quote(temp[0]);
     exit(127);
 }
 

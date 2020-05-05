5a9825bda89f2dba24bfb6b23821312493c3e6ba
ndykyy <ndykyy@student.ucode.world>
2020-05-05 15:27:51 +0300

    pwd chngd

--- src/mx_builtin_pwd.c
+++ src/mx_builtin_pwd.c
@@ -4,6 +4,7 @@ static void part_for_link(char *path)  {
     char *link = mx_strnew(1024);
 
     readlink(path, link, 1024);
+    mx_printstr("/");
     mx_printstr(link);
     mx_printstr("\n");
     mx_strdel(&link);
@@ -23,21 +24,19 @@ static void print_pwd(char *cwd) {
 }
 
 void mx_builtin_pwd(char **arr, t_trig *trig) {
-    char *cwd = trig->PWD;
     t_pwd *pwd = (t_pwd *)malloc(sizeof(t_pwd));
     struct stat lt;
 
     memset(pwd, 0, sizeof(t_pwd));
     mx_parser_4_pwd(arr, pwd, trig);
     if (!pwd->stop) {
-        lstat(cwd, &lt);
-        if (pwd->P) {
-            if ((lt.st_mode & MX_IFMT) == MX_IFLNK)
-                part_for_link(cwd);
-            else
-                print_pwd(cwd);
-        }
+        if (pwd->P)
+            lstat(trig->PWD, &lt);
         else
-            print_pwd(cwd);
+            stat(trig->PWD, &lt);
+        if ((lt.st_mode & MX_IFMT) == MX_IFLNK)
+            part_for_link(trig->PWD);
+        else
+            print_pwd(trig->PWD);
     }
 }

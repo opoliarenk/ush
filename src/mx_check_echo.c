334c2b6026f5243efcd5ffb8956f31b85d8b74c1
Olena Poliarenko <opoliarenk@e2r6p6.unit.ua>
2020-05-06 11:19:39 +0300

    sub.odd.number

--- src/mx_check_echo.c
+++ src/mx_check_echo.c
@@ -26,16 +26,50 @@ static int second(char *line, int *i) {
     return 1;
 }
 
+static int brackets(char *line, int *i) {
+    (*i)++;
+    for (; line[(*i)] != ')'; (*i)++) {
+        if (line[(*i)] == '\0') {
+            dprintf(2, "Odd number of brackets.\n");
+            return 0;
+        }
+        if (line[(*i)] == '\x5c')
+            (*i)++;
+    }
+    return 1;
+}
+
+static int acute(char *line, int *i) {
+    (*i)++;
+    for (; line[(*i)] != '`'; (*i)++) {
+        if (line[(*i)] == '\0') {
+            dprintf(2, "Odd number of acutes.\n");
+            return 0;
+        }
+        if (line[(*i)] == '\x5c')
+            (*i)++;
+    }
+    return 1;
+}
+
 int mx_check_echo(char *line) {
     for (int i = 0; line[i]; i++) {
-        if (line[i] == '\x22') {
-            if (!first(line, &i))
+        if (line[i] == '\x22' || line[i] == '\x27') {
+            if (line[i] == '\x22' && !first(line, &i))
+                return 0;
+            else if (line[i] == '\x27' && !second(line, &i))
                 return 0;
         }
-        else if (line[i] == '\x27') {
-            if (!second(line, &i))
+        else if (line[i] == '$' && line[i + 1] == '(') {
+            if (!brackets(line, &i))
                 return 0;
         }
+        else if (line[i] == '`') {
+            if (!acute(line, &i))
+                return 0;
+        }
+        else if (line[i] == '\x5c')
+            i++;
     }
     return 1;
 }

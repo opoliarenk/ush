52465b0d0d40aacdc0e6b448640c619934155bca
Nazar <ndykyy@iMac-Nazar.local>
2020-05-18 07:28:38 -0700

    some fix

--- src/mx_fill_trig.c
+++ src/mx_fill_trig.c
@@ -1,9 +1,16 @@
 #include "../inc/ush.h"
 
 void mx_fill_trig(t_trig *trig) {
+	char dir[1024];
+	getcwd(dir, 1024);
+
+    if (getenv("_") == NULL)
+        setenv("_", "/usr/bin/env", 1);
+    if (getenv("PATH") == NULL)
+        setenv("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin", 1);
     trig->exit = true;
     trig->err = 0;
-    trig->OLDPWD = strdup(getenv("PWD"));
-    trig->PWD = strdup(getenv("PWD"));
+    trig->OLDPWD = strdup(dir);
+    trig->PWD = strdup(dir);
     trig->pids = NULL;
 }

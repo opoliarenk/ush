96fbcbd8bd6f40e46e5e23124f57d5d72683b7be
Nazar <ndykyy@iMac-Nazar.local>
2020-05-20 08:30:14 -0700

    last com

--- src/mx_fill_trig.c
+++ src/mx_fill_trig.c
@@ -4,10 +4,9 @@ void mx_fill_trig(t_trig *trig) {
 	char dir[1024];
 	getcwd(dir, 1024);
 
-    if (getenv("_") == NULL)
-        setenv("_", "/usr/bin/env", 1);
     if (getenv("PATH") == NULL)
         setenv("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin", 1);
+    setenv("_", "/usr/bin/env", 1);
     trig->exit = true;
     trig->err = 0;
     trig->OLDPWD = strdup(dir);
